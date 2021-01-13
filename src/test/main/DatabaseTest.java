8c296501d3736a35c4f544dc3f5b6ca8576e8804
mage1711 <ziadaha@hotmail.com>
2021-01-13 22:20:30 +0200

    added tests to DatabaseTest

--- src/test/main/DatabaseTest.java
+++ src/test/main/DatabaseTest.java
@@ -1,13 +1,43 @@
-package test.main;
+package main;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
+    private MongoClient client;
+    private MongoDatabase database;
+    private String connectionString = "mongodb+srv://admin:90ZVui6wnRLIL2e9@Cluster1.kocpj.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+    @BeforeEach
+    void setUp() {
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+        this.client = new MongoClient(new MongoClientURI(this.connectionString));
+
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
 
     @Test
     void databaseConnection() {
-
+        MongoIterable<String> mc = client.listDatabaseNames();
+        MongoCursor<String> cursor = mc.cursor();
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
     }
+
 }
