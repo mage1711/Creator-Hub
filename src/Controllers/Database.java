3792aa56377746f7b16dccb4585fb51b70350542
mage1711 <ziadaha@hotmail.com>
2021-01-14 02:41:10 +0200

    added changes to database class

--- src/Controllers/Database.java
+++ src/Controllers/Database.java
@@ -2,33 +2,45 @@ package Controllers;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Database {
-    public void DatabaseConnection(){
+    private MongoClient client;
+    private String connectionString = "mongodb+srv://admin:90ZVui6wnRLIL2e9@Cluster1.kocpj.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    private MongoDatabase database;
+    private String databaseName = "CreatorHub";
+
+    Database() {
+        DatabaseConnection();
+        ConnectToDatabase();
+    }
+
+    public void DatabaseConnection() {
         Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
-            String connectionString = "mongodb+srv://admin:90ZVui6wnRLIL2e9@Cluster1.kocpj.mongodb.net/<dbname>?retryWrites=true&w=majority";
+        try {
+            this.client = new MongoClient(new MongoClientURI(this.connectionString));
 
-        try (MongoClient client = new MongoClient(new MongoClientURI(connectionString)))
-        {
-
-            MongoIterable<String> mc = client.listDatabaseNames();
-            MongoCursor<String> cursor = mc.cursor();
-            while (cursor.hasNext()) {
-                System.out.println(cursor.next());
-            }
-
-
-        }
-
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Cannot connect:" + e);
-
         }
+    }
+
+    public void ConnectToDatabase() {
+        this.database = client.getDatabase(databaseName);
+    }
+
+    public MongoCollection GetCollection(String collectionName){
+        return database.getCollection(collectionName);
+    }
+
+    public void GetAllDocuments(MongoCollection collection){
+        FindIterable results = collection.find();
+
     }
 }
 
--- src/test/main/DatabaseTest.java
+++ src/test/main/DatabaseTest.java
@@ -1,4 +1,4 @@
-package test.main;
+package main;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
